f0c5c9591477a9b93ea70cf74793fb3f718ce3df
Ayse Erdogan <aerdogan@AErdogan-Fyler.attlocal.net>
2023-08-20 10:01:39 -0500

    Hello World 08/20

--- hello.js
+++ hello.js
@@ -4,19 +4,29 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello World!");
 
 /*  
 Next, PLAY! 
-Modify your code on line 7 to try to accomplish the tasks listed below:
+Modify your code on line 7 to try to accomplish the tasks listed below:*/
+// 1. Change the message that is printed.
+console.log("Welcome to the LaunchCode!");
+// 2. Figure out what the parentheses do. Will the code work without them?
+//console.log"Hello World!";
+// 3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+//console.log(Hello World!);
+// 4. Remove the semi-colon, ;.
+//console.log("Hello World!")
+// 5. Print a number. (Bonus: Print two numbers added together).
+console.log("1");
+console.log("13");
+// 6. Print multiple messages one after the other.
+console.log("Hello World!");
+console.log("Welcome to the LaunchCode!");
+// 7. Print two messages on the same line.
+console.log("Hello World! Welcome to the LaunchCode!");
+// 8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+console.log("Hello World! \"Welcome to the LaunchCode!\"");
+//9. Other. You choose!
+console.log("\nHello World! \n\"Welcome to the LaunchCode!\" ");
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
-*/
